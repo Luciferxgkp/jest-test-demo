18c3f91be0812b8f88258d4e093d436970aac1d7
Gourav Kumar <gourav.kumar@thoughtworks.com>
2024-03-08 11:10:30 +0530

    parameterized test

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,21 @@
 export function toUpperCase(arg: String) {
   return arg.toUpperCase();
 }
+
+export type StringInfo = {
+  lowercase: String;
+  uppercase: String;
+  length: Number;
+  characters: String[];
+  extraInfo: Object | undefined;
+};
+
+export function getStringInfo(args: String): StringInfo {
+  return {
+    lowercase: args.toLowerCase(),
+    characters: Array.from(args),
+    uppercase: args.toUpperCase(),
+    length: args.length,
+    extraInfo: {},
+  };
+}
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,9 +1,58 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("should return uppercase", () => {
-    const result = toUpperCase("abc");
+    const expected = "ABC";
 
-    expect(result).toBe("ABC");
+    const actual = toUpperCase("abc");
+
+    expect(actual).toBe(expected);
+  });
+
+  describe("getStringInfo for args should", () => {
+    it("return right length", () => {
+      const actual = getStringInfo("My-string");
+
+      expect(actual.characters).toHaveLength(9);
+    });
+
+    it("return right lowercase", () => {
+      const actual = getStringInfo("My-string");
+
+      expect(actual.lowercase).toBe("my-string");
+    });
+
+    it("return right uppercase", () => {
+      const actual = getStringInfo("My-string");
+
+      expect(actual.uppercase).toBe("MY-STRING");
+    });
+
+    it("return defined extra info", () => {
+      const actual = getStringInfo("My-string");
+
+      expect(actual.extraInfo).toBeDefined();
+    });
+  });
+
+  describe("ToUppercase examples", () => {
+    it.each([
+      {
+        input: "abc",
+        expected: "ABC",
+      },
+      {
+        input: "My-string",
+        expected: "MY-STRING",
+      },
+      {
+        input: "def",
+        expected: "DEF",
+      },
+    ])("$input toUpperCase should be $expected", ({ input, expected }) => {
+      const actual = getStringInfo(input);
+
+      expect(actual.uppercase).toBe(expected);
+    });
   });
 });
