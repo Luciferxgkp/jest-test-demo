6d0fc86abf97a20f5c39ca9a18822f498417ba19
Gourav Kumar <gourav.kumar@thoughtworks.com>
2024-03-11 15:18:37 +0530

    mocks

--- src/app/doubles/Utils.ts
+++ src/app/doubles/Utils.ts
@@ -5,7 +5,20 @@ export type StringInfo = {
   characters: String[];
   extraInfo: Object | undefined;
 };
-
+type LoggerServiceCallBack = (arg: string) => void;
 export function calculateComplexity(stringInfo: StringInfo) {
   return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
+}
+
+export function toUpperCaseWithCb(
+  args: string,
+  callback: LoggerServiceCallBack
+) {
+  if (!args) {
+    callback("Invalid argument!");
+    return;
+  }
+
+  callback(`called function with ${args}`);
+  return args.toUpperCase();
 }
--- src/test/doubles/Utils.test.ts
+++ src/test/doubles/Utils.test.ts
@@ -1,6 +1,54 @@
-import { calculateComplexity } from "../../app/doubles/Utils";
+import {
+  calculateComplexity,
+  toUpperCaseWithCb,
+} from "../../app/doubles/Utils";
 
 describe("Doubles utils test suite", () => {
+  describe("Tracking callbacks with jest mocks", () => {
+    const callBackMock = jest.fn();
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+    it("calls callback for invalid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("", callBackMock);
+      expect(callBackMock).toBeCalledWith("Invalid argument!");
+      expect(callBackMock).toBeCalledTimes(1);
+    });
+
+    it("calls callback for valid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("abc", callBackMock);
+      expect(callBackMock).toBeCalledWith("called function with abc");
+      expect(callBackMock).toBeCalledTimes(1);
+    });
+  });
+
+  describe("Tracking callbacks without jest mocks", () => {
+    let cbArgs = [];
+    let timesCalled = 0;
+    function callBackMock(arg: string) {
+      cbArgs.push(arg);
+      timesCalled++;
+    }
+
+    afterEach(() => {
+      cbArgs = [];
+      timesCalled = 0;
+    });
+
+    it("calls callback for invalid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("", callBackMock);
+      expect(cbArgs).toContain("Invalid argument!");
+      expect(timesCalled).toBe(1);
+    });
+
+    it("calls callback for valid argument - track calls", () => {
+      const actual = toUpperCaseWithCb("abc", callBackMock);
+      expect(cbArgs).toContain("called function with abc");
+      expect(timesCalled).toBe(1);
+    });
+  });
+
   it("Calculate complexity", () => {
     const someInfo = {
       length: 5,
