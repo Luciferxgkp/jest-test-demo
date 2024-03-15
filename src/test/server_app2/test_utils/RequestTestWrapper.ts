273d9e8c4e4cd0590a79304e7b641d6843e4e030
Gourav Kumar <gourav.kumar@thoughtworks.com>
2024-03-15 16:44:02 +0530

    add: low mock

---
+++ src/test/server_app2/test_utils/RequestTestWrapper.ts
@@ -0,0 +1,21 @@
+import { HTTP_METHODS } from "../../../server_app/model/ServerModel";
+
+export class RequestTestWrapper {
+  public body: object;
+  public method: HTTP_METHODS;
+  public url: string;
+  public headers = {};
+
+  public on(event: any, cb: any) {
+    if (event == "data") {
+      cb(JSON.stringify(this.body));
+    } else cb();
+  }
+
+  public clearFields() {
+    this.body = undefined;
+    this.method = undefined;
+    this.url = undefined;
+    this.headers = {};
+  }
+}
